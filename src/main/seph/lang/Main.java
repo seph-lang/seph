3ec9bc970bb23a2d4e8ff0e67e01353b1727cfb4
Ola Bini <ola.bini@gmail.com>
2011-04-23 22:06:37 -0500

    Add more proper version capabilities to make it possible to push drops of Seph to website

--- src/main/seph/lang/Main.java
+++ src/main/seph/lang/Main.java
@@ -6,18 +6,79 @@ package seph.lang;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 
+import java.util.Properties;
+
 /**
  * @author <a href="mailto:ola.bini@gmail.com">Ola Bini</a>
  */
 public class Main {
+    private final static String HELP =
+        "Usage: seph [switches] -- [programfile] [arguments]\n" +
+        " -h, --help      help, this message\n" +
+        " --copyright     print the copyright\n" +
+        " --version       print current version\n";
+
     public static void main(String[] args) throws Throwable {
-        Runtime r = new Runtime();
-        String file = args[0];
-        r.evaluateFile(file);
+        int start = 0;
+        boolean done = false;
+        boolean printedSomething = false;
+
+        for(;!done && start<args.length;start++) {
+            String arg = args[start];
+            if(arg.length() > 0) {
+                if(arg.charAt(0) != '-') {
+                    done = true;
+                    break;
+                } else {
+                    if(arg.equals("--")) {
+                        done = true;
+                    } else if(arg.equals("-h") || arg.equals("--help")) {
+                        System.err.print(HELP);
+                        return;
+                    } else if(arg.equals("--version")) {
+                        System.err.println(getVersion());
+                        printedSomething = true;
+                    } else if(arg.equals("--copyright")) {
+                        System.err.print(COPYRIGHT);
+                        printedSomething = true;
+                    }
+                }
+            }
+        }
+
+        if(args.length > start) {
+            String file = args[start];
+            if(file.startsWith("\"")) {
+                file = file.substring(1, file.length());
+            }
+
+            if(file.length() > 1 && file.charAt(file.length()-1) == '"') {
+                file = file.substring(0, file.length()-1);
+            }
+
+            Runtime r = new Runtime();
+            r.evaluateFile(file);
+        }
+    }
+
+    public static String getVersion() {
+        try {
+            Properties props = new Properties();
+            props.load(Main.class.getResourceAsStream("/seph/lang/version.properties"));
+
+            String version = props.getProperty("seph.build.versionString");
+            String date = props.getProperty("seph.build.date");
+            String commit = props.getProperty("seph.build.commit");
+
+            return version + " [" + date + " -- " + commit + "]";
+        } catch(Exception e) {
+        }
+
+        return "";
     }
 
     private final static String COPYRIGHT =
-        "Copyright (c) 2010 Ola Bini, ola.bini@gmail.com\n"+
+        "Copyright (c) 2010-2011 Ola Bini, ola.bini@gmail.com\n"+
         "\n"+
         "Permission is hereby granted, free of charge, to any person obtaining a copy\n"+
         "of this software and associated documentation files (the \"Software\"), to deal\n"+
