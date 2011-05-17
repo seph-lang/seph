091c9e342abb8bcfc7a76a4c0fa3071bfc4f4239
Ola Bini <ola.bini@gmail.com>
2011-05-17 19:53:03 +0530

    Add a very simple benchmarking tool

--- src/main/seph/lang/Base.java
+++ src/main/seph/lang/Base.java
@@ -96,6 +96,68 @@ public class Base implements SephObject {
         }
     }
 
+    // TODO: move this into an extension once extensions exist
+    // Takes one, two, three or four arguments:
+    //    benchmark(code_to_bench)                       ; will default to repetitions = 10, loops = 1
+    //    benchmark(42, code_to_bench)                   ; will default to loops = 1
+    //    benchmark(42, 2, code_to_bench)                ; repetitions = 42, loops = 2
+    //    benchmark("code one", 42, 2, code_to_bench)    ; repetitions = 42, loops = 2, label "code one"
+    @SephMethod(evaluateArguments=false)
+    public final static SephObject benchmark(SThread thread, LexicalScope scope, IPersistentList args) {
+        int count = args.count();
+        int repetitions = 10;
+        int iterations = 1;
+        String label = null;
+        ISeq argsLeft = args.seq();
+        Object code = null;
+        if(count == 4) {
+            label = ((Text)ControlDefaultBehavior.evaluateArgument(argsLeft.first(), scope, thread, true)).text();
+            argsLeft = argsLeft.next();
+        }
+        if(count > 1) {
+            repetitions = ((gnu.math.IntNum)ControlDefaultBehavior.evaluateArgument(argsLeft.first(), scope, thread, true)).intValue();
+            argsLeft = argsLeft.next();
+        }
+        if(count > 2) {
+            iterations = ((gnu.math.IntNum)ControlDefaultBehavior.evaluateArgument(argsLeft.first(), scope, thread, true)).intValue();
+            argsLeft = argsLeft.next();
+        }
+        code = argsLeft.first();
+
+        String separatorType = System.getProperty("seph.benchmark.format");
+        if(separatorType == null || separatorType.equals("")) {
+            separatorType = "human";
+        }
+
+        
+        for(int i=0;i<repetitions;i++) {
+            long before = System.nanoTime();
+            for(int j=0;j<iterations;j++) {
+                ControlDefaultBehavior.evaluateArgument(code, scope, thread, true);
+            }
+            long after = System.nanoTime();
+            long time = after-before;
+            long secs = time/1000000000;
+            long rest = time%1000000000;
+
+            if(separatorType.equals("human")) {
+                if(label != null) {
+                    System.out.println(String.format("%-32.32s %.6s.%09d", label, secs, rest));
+                } else {
+                    System.out.println(String.format("%.6s.%09d", secs, rest));
+                }
+            } else {
+                if(label != null) {
+                    System.out.println(String.format("%s|%.6s.%09d", label, secs, rest));
+                } else {
+                    System.out.println(String.format("|%.6s.%09d", secs, rest));
+                }
+            }
+        }
+        
+        return Runtime.NIL;
+    }
+
     @SephMethod(name="==")
     public final static SephObject eq(SephObject receiver, SephObject other) {
         if(receiver.equals(other)) {
